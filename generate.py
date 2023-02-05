c9871d1a7b4c44199346d754a1846bc0962dead4
Alexander Weigl <wadoon@users.noreply.github.com>
2023-02-05 03:09:03 +0000

    clean

--- generate.py
+++ generate.py
@@ -6,11 +6,15 @@ import os
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
 from pathlib import Path
+import markdown
+
 
 TARGET = Path("./pull-requests")
 
 env = Environment(loader=FileSystemLoader("templates"),
                   autoescape=select_autoescape())
+
+env.filters['markdown'] = markdown.markdown
 
 PULL_REQUESTS = []
 ARTIFACTS = []
@@ -39,11 +43,44 @@ def generate_pull_request(pull_request: Path):
     target.parent.mkdir(exist_ok=True)
 
     artifacts = [generate_artifact(pull_request, arti_folder)
-                 for arti_folder in pull_request.glob("*/") 
-                 if arti_folder.is_dir() ]
+                 for arti_folder in pull_request.glob("*/")
+                 if arti_folder.is_dir()]
+
+    total_runtimes = {arti_folder.name:  total_runtime(arti_folder)
+                      for arti_folder in pull_request.glob("*/")
+                      if arti_folder.is_dir()}
+
+    keys = sorted(list(total_runtimes.keys()))
+
+    runtime_chart = {'type': 'bar',
+                     'data': {
+                         'labels': keys,
+                             'datasets': [{
+                                 'label': 'runtime',
+                                 'data': [total_runtimes[k][0] for k in keys],
+                                 'borderWidth': 1
+                             },
+                             {
+                                 'label': 'test cases',
+                                 'data': [total_runtimes[k][1] for k in keys],
+                                 'borderWidth': 1
+                             },
+                             ]
+                     },
+                     'options': {
+                         'scales': {
+                             'y': {
+                                 'beginAtZero': True
+                             }
+                         }
+                     }
+                     }
+
+
 
     with target.open('w') as fp:
-        fp.write(template.render(pr=data, artifacts=artifacts))
+        fp.write(template.render(
+            pr=data, artifacts=artifacts, runtimes=total_runtimes, rtchart=runtime_chart))
 
     data.artifacts = artifacts
     return data
@@ -58,9 +95,25 @@ def find_files(directory, pattern):
                 yield filename
 
 
+def total_runtime(folder: Path):
+    from junitparser import JUnitXml, TestCase, TestSuite
+    total_time = 0.0
+    number_test_cases = 0
+    print(list(folder.rglob("*")))
+
+    for f in folder.rglob("*.xml"):
+        xml = JUnitXml.fromfile(f)
+        for suite in xml:
+            case: TestCase
+            for case in suite:
+                total_time += case.time
+                number_test_cases += 1
+    return total_time, number_test_cases
+
+
 def generate_artifact(pull_request, artifact: Path):
     template = env.get_template("artifact.html")
-    target = artifact / "index.md"
+    target = artifact / "index.html"
     data = read_meta(artifact)
     ARTIFACTS.append(data)
 
@@ -76,10 +129,11 @@ def generate_artifact(pull_request, artifact: Path):
     with target.open('w') as fp:
         fp.write(
             template.render(pr=pull_request, artifact=data, indexes=indexes))
+    return data
 
 
 if __name__ == '__main__':
-    pr:Path
+    pr: Path
     for pr in TARGET.glob("*/"):
         if pr.is_dir():
             try:
