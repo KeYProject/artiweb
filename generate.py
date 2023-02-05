66455f6113d8dfd9f998e9ddca0bade270e43179
Alexander Weigl <wadoon@users.noreply.github.com>
2023-02-05 14:04:57 +0000

    charts!

--- generate.py
+++ generate.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+from collections import namedtuple
 import addict
 import json
 import os
@@ -46,41 +47,97 @@ def generate_pull_request(pull_request: Path):
                  for arti_folder in pull_request.glob("*/")
                  if arti_folder.is_dir()]
 
-    total_runtimes = {arti_folder.name:  total_runtime(arti_folder)
+    total_runtimes = {arti_folder.name:  junit_statistics(arti_folder)
                       for arti_folder in pull_request.glob("*/")
                       if arti_folder.is_dir()}
 
     keys = sorted(list(total_runtimes.keys()))
+    values = [total_runtimes[x] for x in keys]
+    labels = [a.created_at
+              for x in total_runtimes.keys()
+              for a in artifacts
+              if str(a.id) == x
+              ]
 
-    runtime_chart = {'type': 'bar',
-                     'data': {
-                         'labels': keys,
-                             'datasets': [{
-                                 'label': 'runtime',
-                                 'data': [total_runtimes[k][0] for k in keys],
-                                 'borderWidth': 1
-                             },
-                             {
-                                 'label': 'test cases',
-                                 'data': [total_runtimes[k][1] for k in keys],
-                                 'borderWidth': 1
-                             },
-                             ]
-                     },
-                     'options': {
-                         'scales': {
-                             'y': {
-                                 'beginAtZero': True
-                             }
-                         }
-                     }
-                     }
+    runtime_chart = {
+        'type': 'bar',
+        'data': {
+            'labels': labels,
+            'datasets': [{
+                'label': 'Runtime in Hours',
+                'data': [v.total_time/60.0/60.0 for v in values],
+                'borderWidth': 1
+            },
+            ]
+        },
+        'options': {
+            'responsive': True,
+            'maintainAspectRatio': False,
+            'scales': {
+                'y': {
+                    'beginAtZero': True
+                }
+            }
+        }
+    }
 
+    test_cases = {
+        'type': 'line',
+        'data': {
+            'labels': labels,
+            'datasets': [
+                {
+                    'label': 'Failures',
+                    'data': [v.failures for v in values],
+                    'fill': True,
+                    'borderColor': 'rgb(250, 192, 192)',
+                    'backgroundColor': 'rgb(250, 220, 220)',
+                    'tension': 0.1,
+                },
+                {
+                    'label': 'Errors',
+                    'data': [v.errors for v in values],
+                    'fill': True,
+                    'borderColor': 'rgb(180, 192, 192)',
+                    'backgroundColor': 'rgb(200, 212, 212)',
+                    'tension': 0.1,
+                },
+                {
+                    'label': 'Skipped',
+                    'data': [v.skipped for v in values],
+                    'fill': True,
+                    'borderColor': 'rgb(100, 100, 100)',
+                    'backgroundColor': 'rgb(150, 150, 150)',
+                    'tension': 0.1,
+                },
+                {
+                    'label': 'Success',
+                    'data': [v.success for v in values],
+                    'fill': True,
+                    'borderColor': 'rgb(75, 192, 100)',
+                    'backgroundColor': 'rgb(95, 212, 120)',
+                    'tension': 0.1,
+                }]
+        },
+        'options': {
+            'responsive': True,
+            'maintainAspectRatio': False,
 
+            'scales': {
+                'y': {
+                    'stacked': True
+                }
+            }
+        }
+    }
 
+    # import pprint
+    # pprint.pprint(test_cases)
+
+    print(target)
     with target.open('w') as fp:
         fp.write(template.render(
-            pr=data, artifacts=artifacts, runtimes=total_runtimes, rtchart=runtime_chart))
+            pr=data, artifacts=artifacts, runtimes=total_runtimes, rtchart=runtime_chart, tcchart=test_cases))
 
     data.artifacts = artifacts
     return data
@@ -95,20 +152,33 @@ def find_files(directory, pattern):
                 yield filename
 
 
-def total_runtime(folder: Path):
+JUnitStat = namedtuple(
+    "JUnitStat", "total_time, total_tests, skipped, errors, failures, success")
+
+
+def junit_statistics(folder: Path):
     from junitparser import JUnitXml, TestCase, TestSuite
     total_time = 0.0
     number_test_cases = 0
-    print(list(folder.rglob("*")))
-
+    skipped = 0
+    error = 0
+    success = 0
+    failures = 0
     for f in folder.rglob("*.xml"):
         xml = JUnitXml.fromfile(f)
-        for suite in xml:
-            case: TestCase
-            for case in suite:
-                total_time += case.time
-                number_test_cases += 1
-    return total_time, number_test_cases
+        total_time += xml.time
+
+        s = xml.skipped
+        t = xml.tests
+        f = xml.failures
+        e = xml.errors
+
+        number_test_cases += t
+        skipped += s
+        error += e
+        failures += f
+        success += (t - f - e - s)
+    return JUnitStat(total_time, number_test_cases, skipped, error, failures, success)
 
 
 def generate_artifact(pull_request, artifact: Path):
@@ -140,4 +210,5 @@ if __name__ == '__main__':
                 generate_pull_request(pr)
             except Exception as e:
                 print(e)
+                raise e
     index()
